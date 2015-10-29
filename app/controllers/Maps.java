5fc9a97e7afa5c5e9f6e85ef14bf910b283d1275
ssachtleben <ssachtleben@gmail.com>
2015-10-29 12:29:26 +0100

    added maps controller

---
+++ app/controllers/Maps.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import play.Logger;
+import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
+import play.mvc.Result;
+
+import java.util.Map;
+
+import static play.libs.Json.toJson;
+
+/**
+ * The Maps controller handle api requests for the Map model.
+ *
+ * @author Sebastian Sachtleben
+ */
+public class Maps extends BaseAPI<Long, Map> {
+
+    private static final Logger.ALogger log = Logger.of(Maps.class);
+
+    public Maps() {
+        super(Long.class, Map.class);
+    }
+
+    @Transactional
+    public Result update(Long id) {
+        Map map = findUnique(id);
+        map = JPA.em().merge(map);
+        return ok(toJson(map));
+    }
+
+}
+
