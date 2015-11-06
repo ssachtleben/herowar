f3ed20cc45ecb1abc51f9cd56b6f9dd2f92a2ae8
ssachtleben <ssachtleben@gmail.com>
2015-11-06 20:38:38 +0100

    added missing editor methods

---
+++ app/utils/EnvironmentComparator.java
@@ -0,0 +1,14 @@
+package utils;
+
+import models.entity.game.Environment;
+
+import java.util.Comparator;
+
+public class EnvironmentComparator implements Comparator<Environment> {
+
+	@Override
+	public int compare(Environment env1, Environment env2) {
+		return env1.getName().compareTo(env2.getName());
+	}
+
+}
