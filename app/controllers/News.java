833e51084a7f95203f53facf928b32e4fc1f8b79
awilhelmer <awilhelmer@gmx.de>
2015-11-07 11:00:22 +0100

    Missing transactional

--- app/controllers/News.java
+++ app/controllers/News.java
@@ -2,6 +2,7 @@ package controllers;
 
 import play.data.Form;
 import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
 import play.mvc.Result;
 
 import static play.libs.Json.toJson;
@@ -12,6 +13,7 @@ public class News extends BaseAPI<Long, models.entity.News> {
       super(Long.class, models.entity.News.class);
    }
 
+   @Transactional
    public Result update(final Long id) {
       // models.entity.News news = instance.findUnique(id);
       models.entity.News news = JPA.em().merge(Form.form(models.entity.News.class).bindFromRequest().get());
