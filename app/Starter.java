d3f40f0780dc76576f5784bb9958c96321adfeb7
awilhelmer <awilhelmer@gmx.de>
2015-11-07 10:21:51 +0100

    fixed Starter

--- app/Starter.java
+++ app/Starter.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
@@ -12,11 +14,11 @@ import java.net.URL;
 public class Starter {
    private final static String CMDSTRING = "./activator";
    private static String OS = System.getProperty("os.name").toLowerCase();
+   private static boolean FIRST_CALL = false;
 
    public static void main(String[] args) {
       try {
          startActivator();
-         loadLocalhost();
       }
       catch (IOException e) {
          e.printStackTrace();
@@ -35,11 +37,29 @@ public class Starter {
       String line;
       while ((line = input.readLine()) != null) {
          System.out.println(line);
+         if (!FIRST_CALL && line.contains("(Server started, use Ctrl+D to stop and go back to the console...)")) {
+            new Thread(() -> loadLocalhost()).start();
+         }
+
       }
    }
 
-   public static void loadLocalhost() throws IOException {
-      new URL("http://localhost:9000").openConnection();
+   public static void loadLocalhost() {
+      if (!FIRST_CALL) {
+         FIRST_CALL = true;
+         System.out.println(String.format("%s", "Try to make first call"));
+         try {
+            HttpURLConnection conn = (HttpURLConnection) new URL("http://localhost:9000").openConnection();
+            InputStream is = conn.getInputStream();
+            is.read();
+
+         }
+         catch (IOException e) {
+            e.printStackTrace();
+         }
+
+      }
+
    }
 
    public static boolean isWindows() {
