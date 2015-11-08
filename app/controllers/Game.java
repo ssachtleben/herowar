f21276f8afdf1cfd5b8a2a631e1cb26e62951836
awilhelmer <awilhelmer@gmx.de>
2015-11-08 16:44:00 +0100

    that class was recompiled every time

--- app/controllers/Game.java
+++ app/controllers/Game.java
@@ -4,47 +4,20 @@ import com.fasterxml.jackson.databind.JsonNode;
 import models.entity.User;
 import network.Connection;
 import play.db.jpa.JPA;
-import play.libs.F;
 import play.mvc.Result;
-import play.mvc.WebSocket;
 import views.html.game;
 
-/**
- * Provides all /game related routes.
- *
- * @author Sebastian Sachtleben
- */
 public class Game extends BaseController {
 
-   /**
-    * Render game index view.
-    * <p>
-    * The game itself is a single page application and don't need additional routes.
-    * </p>
-    *
-    * @return The {@link Result} object.
-    */
    public Result index() {
       return ok(game.render());
    }
 
-   /**
-    * Provides socket connection for exchanging game data.
-    * <p>
-    * This {@link play.mvc.WebSocket} only allow to send and receive {@link JsonNode} objects. Any other object type will result discoonect
-    * the connection.
-    * </p>
-    *
-    * @return The {@link play.mvc.WebSocket} object.
-    */
    public play.mvc.WebSocket<JsonNode> data() {
       try {
-         return JPA.withTransaction(new F.Function0<WebSocket<JsonNode>>() {
-            @Override
-            public play.mvc.WebSocket<JsonNode> apply() throws Throwable {
-               final User user = Application.getLocalUser(session());
-               return new Connection(request().remoteAddress(), user);
-            }
+         return JPA.withTransaction(() -> {
+            final User user = Application.getLocalUser(session());
+            return new Connection(request().remoteAddress(), user);
          });
       }
       catch (Throwable e) {
