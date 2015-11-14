f4783ed97a7567f542177c6e27afd62f1b7152b9
ssachtleben <ssachtleben@gmail.com>
2015-11-14 12:31:21 +0100

    improved logged in user code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,7 +4,6 @@ import com.ssachtleben.play.plugin.auth.Auth;
 import dao.UserDAO;
 import models.entity.User;
 import org.apache.commons.lang3.math.NumberUtils;
-import play.Play;
 import play.mvc.Http;
 import play.mvc.Result;
 import views.html.admin;
@@ -12,22 +11,34 @@ import views.html.editor;
 import views.html.game;
 import views.html.site;
 
+/**
+ * Main controller to handle all major site requests. Also allow to retrieve the current logged in user.
+ *
+ * @author Sebastian Sachtleben
+ */
 public class Application extends BaseController {
 
    public static final String USER_ROLE = "user";
    public static final String ADMIN_ROLE = "admin";
-   public static final String ADMIN_ROLE2 = "admin22";
 
    public static String[] roles = { USER_ROLE, ADMIN_ROLE };
 
+   /**
+    * Retrieves the logged in user from the given session.
+    *
+    * @param session The http session
+    * @return The current logged in user.
+    */
    public static User getLocalUser(final Http.Session session) {
-      final String userId = Auth.getLoggedIn(session());
-      UserDAO userDAO = Play.application().injector().instanceOf(UserDAO.class);
-      final User user = NumberUtils.isNumber(userId) ? userDAO.getById(Long.parseLong(userId)) : null;
-      Play.application().injector().instanceOf(Application.class).log().debug(String.format("User: %s", user));
-      return user;
+      final String userId = Auth.getLoggedIn(session);
+      return NumberUtils.isNumber(userId) ? UserDAO.instance().getById(Long.parseLong(userId)) : null;
    }
 
+   /**
+    * Retrieves the logged in user. Works only from controller scope.
+    *
+    * @return The current logged in user.
+    */
    public static User getLocalUser() {
       return getLocalUser(session());
    }
